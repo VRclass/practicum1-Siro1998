d2a994029c7518f3083ba21768d4d8fcfe5ec39f
Siro Zeng <sirozeng@2017-Siro-Zeng.local>
2019-03-11 12:47:32 -0500

    Practicum1
    I created an outdoor environment of a garden and a road. There are a table, several lights, a tree, a box and a sphere in the garden. The box can be picked up, and the sphere is changing color. The color of the skybox is changed to a warmer color and the sun is rotating to show the time changing.  There are five sounds added to the environment. The sound of rain, wind, tree and two lights.

--- Assets/Practicum/Scripts/Translation.cs
+++ Assets/Practicum/Scripts/Translation.cs
@@ -19,8 +19,8 @@ public class Translation : MonoBehaviour {
 		// transform.Translate(Vector3.up * moveSpeed * Time.deltaTime, Space.Self);
 //
 //		transform.Translate(Vector3.up * Time.deltaTime);
-//		transform.Translate(Vector3.up * Time.deltaTime, Space.World);
-		transform.Translate(Vector3.forward * .03f);
+		transform.Translate(Vector3.up * Time.deltaTime, Space.World);
+//		transform.Translate(Vector3.forward * .03f);
 		// check other options such as Vector3.forward, etc. 
 
 		// transform.Translate(Time.deltaTime, 0, 0, Camera.main.transform);
